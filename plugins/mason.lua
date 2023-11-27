8368eab45906c2f4eed1249a3c9371f13ad203ed
Evgeniy V Abashin <abashin@alfasatcom.ru>
2023-11-27 10:20:29 +0300

    feat: добавляет настройки плагинов mason и treesitter

---
+++ plugins/mason.lua
@@ -0,0 +1,17 @@
+return {
+  -- tools
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "html-lsp",
+        "css-lsp",
+        "json-lsp",
+        "bash-language-server",
+        "tailwindcss-language-server",
+        "typescript-language-server",
+        "vue-language-server",
+      })
+    end,
+  },
+}
---
+++ plugins/treesitter.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "javascript",
+        "html",
+        "scss",
+        "vue",
+        "bash",
+        "json",
+        "markdown",
+      },
+    },
+  },
+}
