ac44006556d8c54716f7b57d0eddf04026830995
Evgeniy V Abashin <abashin@alfasatcom.ru>
2023-11-27 12:50:34 +0300

    fix: устраняет ошибки при автоформатировании (убирает часть lsp)

--- plugins/lsp.lua
+++ plugins/lsp.lua
@@ -8,6 +8,12 @@ return {
           require("lazyvim.util").lsp.on_attach(function(client)
             if client.name == "eslint" then
               client.server_capabilities.documentFormattingProvider = true
+            elseif client.name == "tsserver" then
+              client.server_capabilities.documentFormattingProvider = false
+            elseif client.name == "vuels" then
+              client.server_capabilities.documentFormattingProvider = false
+            elseif client.name == "volar" then
+              client.server_capabilities.documentFormattingProvider = false
             elseif client.name == "tailwindcss" then
               client.server_capabilities.documentFormattingProvider = false
             end
