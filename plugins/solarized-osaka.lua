6e442a5766ef8e557fd851c2263bd093b7443ceb
Evgeniy Abashin <abashin@Evgeniys-MacBook-Air.local>
2024-07-07 20:53:01 +0300

    feat: добавляет прозрачный фон темы solarized-osaka

--- plugins/solarized-osaka.lua
+++ plugins/solarized-osaka.lua
@@ -1,22 +1,17 @@
 return {
-	{
-		"craftzdog/solarized-osaka.nvim",
-		lazy = true,
-		priority = 1000,
-		opts = function()
-			return {
-				transparent = false,
-			}
-		end,
-	},
-	{
-		"LazyVim/LazyVim",
-		opts = {
-			colorscheme = "solarized-osaka",
-			news = {
-				lazyvim = true,
-				neovim = true,
-			},
-		},
-	},
+  {
+    "craftzdog/solarized-osaka.nvim",
+    lazy = true,
+    priority = 1000,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "solarized-osaka",
+      news = {
+        lazyvim = true,
+        neovim = true,
+      },
+    },
+  },
 }
